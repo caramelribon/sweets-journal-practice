82577cea6692b5c39275708b9f22762e09e61f71
caramelribon <caramelribon137@gmail.com>
2022-10-15 11:14:58 +0900

    AppHeaderの作成
    user buttonの見た目の作成

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="z-1">
+    <nav>
+      <div class="flex justify-end items-center w-full px-3 py-3">
+        <div class="relative">
+          <!-- User Button (ユーザボタン) -->
+          <button><i class="far fa-user fa-2x user-btn-color"></i></button>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.user-btn-color {
+  color: #8d8985;
+}
+</style>
